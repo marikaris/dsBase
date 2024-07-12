ddc5e6c7741075a806cf7814fecb1664be5740bf
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-12 11:59:28 +0200

    feat: added serverside mem_used function

---
+++ R/memUsedDS.R
@@ -0,0 +1,11 @@
+#' @title Returns memory used in R session
+#' @description This function is similar to R function \code{pryr::mem_used}.
+#' @return The memory used in the R session
+#' @author Tim Cadman
+#' @importFrom pryr mem_used
+#' @export
+memUsedDS <- function(){
+  return(pryr::mem_used())
+}
+#AGGREGATE FUNCTION
+# classDS
